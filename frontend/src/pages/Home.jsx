692d2b779a82225da136f2eca50c1b5627f96a33
CodeDragi <codedragi@gmail.com>
2022-04-08 19:08:22 +0200

    Home Page
    Build Home Page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,22 @@
 import React from 'react'
-
+import { OutlineButton } from '../components/button/Button';
+import { Link } from 'react-router-dom'
 import HeroSlide from '../components/hero-slide/HeroSlide';
 
 const Home = () => {
   return (
     <React.Fragment>
       <HeroSlide/>
+      <div className="container">
+        <div className="section mb-3">
+          <div className="section__header mb-2">
+            <h2>Tendance</h2>
+            <Link>
+              <OutlineButton className="small">Voir Plus</OutlineButton>
+            </Link>
+          </div>
+        </div>
+      </div>
     </React.Fragment>
   )
 }
