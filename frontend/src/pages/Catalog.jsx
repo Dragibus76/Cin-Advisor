6dc04d90659bdfe20925f154181ca4aa5314bd27
CodeDragi <codedragi@gmail.com>
2022-04-09 13:39:20 +0200

    Catalog
    Build Catalog

--- frontend/src/pages/Catalog.jsx
+++ frontend/src/pages/Catalog.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
 
+import { useParams } from 'react-router-dom';
+
+import PageHeader from '../components/page-header/PageHeader';
+
+import { category as cate } from '../api/tmdbApi';
+
 const Catalog = () => {
+
+  const { category } = useParams();
+
+  console.log(category);
   return (
-    <div>Catalog</div>
+    <React.Fragment>
+      <PageHeader>
+        {category === cate.movie ? 'Films' : 'Series TV'}
+      </PageHeader>
+    </React.Fragment>
   )
 }
 
